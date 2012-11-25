d7a8d4f0064a815d1cc0c2356c6eefec0b420db2
Cameron Johnston <cameron@needle.com>
2012-11-24 20:48:17 -0700

    adding execute resource to jenkins recipe for seeding github into ssh known_hosts

--- recipes/jenkins.rb
+++ recipes/jenkins.rb
@@ -8,3 +8,10 @@ users_manage 'jenkins' do
     group_id 2600
     action [ :remove, :create ]
 end
+
+execute 'add github to known_hosts' do
+  user 'jenkins'
+  group 'jenkins'
+  command "ssh-keyscan -H github.com > #{node['jenkins']['server']['home']}/.ssh/known_hosts"
+  not_if { ::File.exists?(node['jenkins']['server']['home'] + '/.ssh/known_hosts') }
+end
