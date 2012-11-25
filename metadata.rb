6ec2862e49f17ee7a589f6d4fce77a637079b665
Cameron Johnston <cameron@needle.com>
2012-11-24 20:49:17 -0700

    bumping cookbook to 0.0.2

--- metadata.rb
+++ metadata.rb
@@ -3,6 +3,6 @@ maintainer_email "ops@needle.com"
 license          "Apache 2.0"
 description      "wrapper cookbook for opscode's users cookbook"
 
-version          "0.1"
+version          "0.0.2"
 
 depends 'users'
